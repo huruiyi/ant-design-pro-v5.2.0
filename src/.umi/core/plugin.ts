926f0ea6c6893dafb11441f7c4dfc7662daf9d46
胡睿毅 <807776962@qq.com>
2022-06-13 20:05:53 +0800

    ant-design-pro-v5.2.0

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'D:/Work/TBK/vue-react/ant-design-pro-v5.2.0/node_modules/_@umijs_runtime@3.5.26@@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','locale','layout','layoutActionRef','request',],
+});
+
+export { plugin };
